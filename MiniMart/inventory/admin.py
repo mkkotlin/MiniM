740922a23567e4a7c7c00bc48dd314aa5c8a8f31
mayank <123.ultron.1234@gmail.com>
2025-06-25 15:22:08 +0530

    admin control filter added

--- MiniMart/inventory/admin.py
+++ MiniMart/inventory/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from . models import Product, Sale
 
 # Register your models here.
-admin.site.register(Product)
 
-admin.site.register(Sale)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','category','stock','cost_price','selling_price','created_at')
+    list_filter = ('name','category','stock','selling_price', 'cost_price','created_at')
+    search_fields = ('name','category')
+# admin.site.register(Product)
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('product','quantity','sale_price', 'date')
+    list_filter = ('product','quantity','product__category', 'date')
+    search_fields = ('product__name',)
+# admin.site.register(Sale)
