0a879e7ae5e434bc6763bbae2aa58bbe03deb152
mayank <123.ultron.1234@gmail.com>
2025-07-05 15:33:12 +0530

    feat: add navigation and sales URL routing, update template settings

--- MiniMart/minimart/urls.py
+++ MiniMart/minimart/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inventory/', include('inventory.urls')),
+    path('sales/', include('sales.urls'))
 ]
