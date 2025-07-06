9d3114870a250949d0ed347181d7f4262255040d
mayank <123.ultron.1234@gmail.com>
2025-07-06 17:28:53 +0530

    feat: update sales template with static file linking and add CSS styles

--- MiniMart/inventory/views.py
+++ MiniMart/inventory/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from urllib3 import request
 from . models import Product
 from django.template.loader import render_to_string
 import json
